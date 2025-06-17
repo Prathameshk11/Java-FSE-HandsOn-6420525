692f51d578fefe837425dfad87bcb82d3cefd4c4
Prathameshk11 <prathameshkhokaralkar@gmail.com>
2025-06-17 19:16:21 +0530

    Implemented Proxy pattern example exe 6

---
+++ Week1_Engineering_concepts/Design_Patterns_and_Principles/exe6/ProxyPatternExample/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        Image image1 = new ProxyImage("photo1.jpg");
+
+        image1.display();
+
+        image1.display();
+
+        Image image2 = new ProxyImage("photo2.jpg");
+        image2.display();
+    }
+}
