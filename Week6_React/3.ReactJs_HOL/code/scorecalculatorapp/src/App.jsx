6d0692740d5341e7270c4b87f6eca42ce715f57e
Prathameshk11 <prathameshkhokaralkar@gmail.com>
2025-07-23 20:56:12 +0530

    Implemented exe 3.ReactJs_HOL : score calculator app

---
+++ Week6_React/3.ReactJs_HOL/code/scorecalculatorapp/src/App.jsx
@@ -0,0 +1,11 @@
+import {CalculateScore} from './Components/CalculateScore';
+
+function App() {
+  return (
+    <div>
+      <CalculateScore Name={"Prathamesh"} School={"DYP, pimpri"} total={284} goal={3} />
+    </div>
+  );
+}
+
+export default App;
